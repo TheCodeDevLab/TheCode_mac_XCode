6dbb368d3171ef35804076f560b1ef53186d7b85
juliette <juliette.debono2002@gmail.com>
2022-07-11 01:55:28 +0200

    reation

---
+++ TheCodeMac/TheCodeAppMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TheCodeAppMac
+//
+//  Created by Juliette Debono on 28/04/2020.
+//  Copyright © 2020 Juliette Debono. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
